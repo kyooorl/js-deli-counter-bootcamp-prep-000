ae8d889f088389c3193a4fcf0aeda8203573e679
Monica Lee <mylee.715@gmail.com>
2019-06-05 18:53:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,14 +12,3 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + name + ".";
   }
 }
-
-function currentLine(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return `The is currently empty.`;
-  } else {
-    for (var i = 0; i < line.length; i++) {
-      katzDeliLine.push(`${i+1}. ${katzDeliLine[i]}`);
-    }
-  return `The line is currently: ` + katzDeliLine;
-  }
-}
